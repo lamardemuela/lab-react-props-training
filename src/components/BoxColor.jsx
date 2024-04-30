22ce0fc516c8d95591fdad37534a3254c2131556
Águeda Muela <agmuelamar@gmail.com>
2024-04-30 15:41:22 +0200

    lab terminado y añadido el hex

--- src/components/BoxColor.jsx
+++ src/components/BoxColor.jsx
@@ -4,13 +4,19 @@ export default function BoxColor(props) {
         margin: "10px",
         padding: "5px",
         display:"flex",
+        flexDirection:"column",
         alignItems: "center",
         justifyContent: "center",
         backgroundColor:`rgb(${props.r}, ${props.g}, ${props.b})`
       };
+      function convertToHex(val) {
+        const hex = val.toString(16).padStart(2, '0');
+        return hex;
+      }
   return (
     <div style= {boxColorStyles} >
         <p>rgb({props.r}, {props.g}, {props.b})</p>
+        <p>#{convertToHex(props.r)}{convertToHex(props.g)}{convertToHex(props.b)}</p>
     </div>
   )
 }
